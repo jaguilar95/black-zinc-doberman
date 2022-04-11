55372e5d53cb2f2667ff46f93d9122cb932fc331
Leonardo <josue.aguilar1995@gmail.com>
2022-04-10 22:07:43 -0500

    loaded first question using start quiz button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,73 @@
 var stage = document.querySelector("#stage");
 var startUpEl = document.createElement("div");
+var questionEl = document.createElement("div");
+var questionsArr = [
+  {
+    question: "What is my favorite color?",
+    alpha: "green",
+    beta: "blue",
+    gamma: "yellow",
+    delta: "black",
+  },
+  {
+    question: "What is my favorite food?",
+    alpha: "tacos",
+    beta: "pizza",
+    gamma: "ramen",
+    delta: "chicken wings",
+  },
+];
 
 var startUp = function () {
   stage.appendChild(startUpEl);
   startUpEl.setAttribute("id", "start-up");
 
-  var startUpTitleEL = document.createElement("h1");
-  startUpTitleEL.textContent = "Coding Quiz Challenge!";
-  startUpEl.appendChild(startUpTitleEL);
+  var startUpTitleEl = document.createElement("h1");
+  startUpTitleEl.textContent = "Coding Quiz Challenge!";
+  startUpEl.appendChild(startUpTitleEl);
 
   var startUpTextEl = document.createElement("p");
   startUpTextEl.textContent =
     "Try to answer the following coding-related questions within one minute. Any incorrect answers will lower your time and final score by ten seconds. Good luck!";
   startUpEl.appendChild(startUpTextEl);
 
-  var startButtonEL = document.createElement("button");
-  startButtonEL.textContent = "Start Quiz!";
-  startButtonEL.addEventListener("click", startQuiz);
-  startUpEl.appendChild(startButtonEL);
+  var startButtonEl = document.createElement("button");
+  startButtonEl.textContent = "Start Quiz!";
+  startButtonEl.addEventListener("click", startQuiz);
+  startUpEl.appendChild(startButtonEl);
 };
 
 var startQuiz = function () {
   startUpEl.remove();
+  loadQuestion();
+};
+
+var loadQuestion = function () {
+  stage.appendChild(questionEl);
+  questionEl.setAttribute("id", "question-container");
+
+  var questionTitleEl = document.createElement("h3");
+  questionTitleEl.textContent = questionsArr[0].question;
+  questionEl.appendChild(questionTitleEl);
+
+  var questionAnswersEl = document.createElement("ul");
+  questionEl.appendChild(questionAnswersEl);
+
+  // answers creation and addition
+  var answerAlphaEl = document.createElement("li");
+  var answerBetaEl = document.createElement("li");
+  var answerGammaEl = document.createElement("li");
+  var answerDeltaEl = document.createElement("li");
+
+  answerAlphaEl.textContent = questionsArr[0].alpha;
+  answerBetaEl.textContent = questionsArr[0].beta;
+  answerGammaEl.textContent = questionsArr[0].gamma;
+  answerDeltaEl.textContent = questionsArr[0].delta;
+
+  questionAnswersEl.appendChild(answerAlphaEl);
+  questionAnswersEl.appendChild(answerBetaEl);
+  questionAnswersEl.appendChild(answerGammaEl);
+  questionAnswersEl.appendChild(answerDeltaEl);
 };
 
 startUp();
