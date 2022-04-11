7af9ed401f829bd2d7ad73f22fe6adc2e95968d8
Leonardo <josue.aguilar1995@gmail.com>
2022-04-10 20:45:46 -0500

    pseudo-code in script.js and divs in index.html

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var startUp = function () {
+  var startUpEL = document.createElement("div");
+  var startUpTitleEL = document.create;
+};
+
+/*
+
+Array that holds all question objects
+
+Function that loads in starting page element
+* Title
+* Descriptive text
+* Start quiz button that leads to first question 
+
+Start quiz function 
+* Deletes starting page
+* Loads first question
+* Starts timer
+
+Function that loads in a question element
+
+Timer function
+* Starts at 90 sections
+* Counts down one section at a time while not zero
+* If correct answer is selected, run correct answer function
+* If incorrect answer is selected, run incorrect answer function
+* Continue to next question
+
+Correct answer function
+* Highlights answer
+* Displays 'Correct!'
+
+Incorrect answer function
+* Highlights correct answer and flags incorrect answer
+* Penalizes incorrect answers
+
+Function that tracks score in localStorage and displays top scores
+
+
+*/
